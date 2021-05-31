6e73d3f9384b42a361d9a4f4b855bed498c18b4f
Alexandr Pogodin <alex.pogodin@gmail.com>
2021-05-31 12:18:20 +0300

    Add `verify` output to check infrastructure

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "environment" {
 output "public-ip" {
   value = azurerm_public_ip.main.ip_address
 }
+
+output "verify" {
+  value = "while true; do curl -s http://${azurerm_public_ip.main.ip_address}; sleep 5; done"
+  description = "Run this one-liner to check load balancing."
+}
